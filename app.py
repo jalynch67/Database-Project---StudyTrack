a3d52b49b7e0eb87b2795ba2f7a952e8a96092eb
Jake Lynch <jakelynch9320@gmail.com>
2026-09-12 11:44:06 +0100

    Add environment configuration and database setup for Flask application

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
+import os
+
+from dotenv import load_dotenv
 from flask import Flask, render_template
 
+from models import db
+
+
+load_dotenv()
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+    "DATABASE_URL",
+    "sqlite://studytrack.db",
+)
+
+db.init_app(app)
 
 
 @app.route("/")
